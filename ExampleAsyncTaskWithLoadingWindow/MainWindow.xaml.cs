6223a170748279c82df40f042393ad68a169e16c
VladislavZverincev <hopmxod007@mail.ru>
2021-10-13 21:47:35 +0700

    Добавьте файлы проекта.

---
+++ ExampleAsyncTaskWithLoadingWindow/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using UniversalLoadingWindowLib;
+
+namespace ExampleAsyncTaskWithLoadingWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Color a = new Color { A = 255, R = 45, G = 70, B = 100 };
+            Color b = new Color { A = 255, R = 70, G = 110, B = 160 };
+            LinearGradientBrush mainbrush = new LinearGradientBrush(a, b, 90);
+            Color title_foreground = new Color { A = 255, R = 180, G = 170, B = 50 };
+            Color annotation_foreground = new Color { A = 255, R = 170, G = 200, B = 225 };
+            string title = "Loading";
+            string annotation = "Pleace, wait few seconds...";
+
+            UniversalLoadingWindow ulw = new UniversalLoadingWindow(title, annotation, mainbrush,
+                title_foreground, title_foreground, annotation_foreground);
+            
+            ulw.Loading(this, () => WorkMethod(ulw));
+        }
+        void WorkMethod(UniversalLoadingWindow ulw)
+        {
+            for (int i = 0; i < 15000000; i++)
+            {
+                ulw.SetPercentProgress(Convert.ToByte((((double)i / (double)15000000)) * 100));
+            }
+        }
+    }
+}
